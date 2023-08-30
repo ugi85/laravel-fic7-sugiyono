bb32ab4d8bd43285286626200820dcf8edc8d53d
Ugi85 <ugijunkies@gmail.com>
2023-08-30 22:03:41 +0700

    api-register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -48,6 +48,28 @@ class AuthController extends Controller
         ]);
     }
 
+    public function register(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required',
+            'name' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'user'
+        ]);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+        return response()->json([
+            'token' => $token,
+            'user' => new UserResource($user),
+        ]);
+    }
+
     public function logout(Request $request)
     {
         $token = $request->bearerToken();
--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/login',[AuthController::class, 'login']);
+Route::post('/register',[AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum'); // Route logout
 
 Route::apiResource('categories', CategoryController::class);
